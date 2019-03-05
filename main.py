fcbafbc5dd0a90fdebedef85b8dee0bb55c42e83
Julian COIGNET <jcoignet@i-tracing.com>
2019-03-05 16:43:17 +0100

    test

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import multidict
+import sharepy
+import xlrd
+import certifi
+import psycopg2-binary
+import pytz
+import django-auth-ldap
+import pyasn1-modules
+import setuptools
+import idna-ssl
+import urllib3
+import chardet
+import django-widget-tweaks
+import aiohttp
+import python-ldap
+import yarl
+import djangorestframework
+import pyasn1
+import attrs
+import idna
+import async-timeout
+import wheel
+import Django
+import pip
+import django-json-widget
+
+if __name__ == '__main__':
+	pass
+
